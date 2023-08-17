26756504f6827e5b5aadb284ee070185c255ddb1
Vini27Br <viniciusouza0@gmail.com>
2023-08-17 08:54:17 -0300

    aula 3

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,18 +1,43 @@
 import React from "react";
-import { Image, StyleSheet  } from "react-native";
-import topo from '../../../assets/topo.png'
+import { Dimensions, Image, StyleSheet, Text, View  } from "react-native";
+import topo from '../../../assets/topo.png';
 
+
+const width = Dimensions.get('screen').width;
 
 export default function Carrinho() {
 
- return <Image source={ topo } style={estilos.topo}/>
+ return <>
+ <Image source={ topo } style={estilos.topo}/>
  
+ <Text style={estilos.titulo}>Detalhes do Carrionho</Text>
+
+ <View>
+    <Text> Carrinho de Compras </Text>   
+    <Text> Fazendinha do incrivel Vinícus </Text>   
+    <Text> Uma cesta com produtos selecionadas com cuidado e carinho</Text>   
+    <Text> R$ 40,00 </Text>   
+ </View>
+    </>
+
 }
 
 const estilos = StyleSheet.create({
 
     topo: {
-        width: "100%"
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "white",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16,
+
     }
 
 })
