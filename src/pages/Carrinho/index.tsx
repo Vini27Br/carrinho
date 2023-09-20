0b40e43c11883bb8b8003e9adcf02bd5b58e3f3e
Vini27Br <viniciusouza0@gmail.com>
2023-09-20 15:07:22 -0300

    aula 6

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { Dimensions, Image, StyleSheet, Text, View  } from "react-native";
-import topo from '../../../assets/topo.png';
+import { Image, StyleSheet, Text, View  } from "react-native";
+import logo from '../../../assets/logo.png';
+
+import Topo from '../components/Topo'
 
 
-const width = Dimensions.get('screen').width;
+
 
 export default function Carrinho() {
 
  return <>
- <Image source={ topo } style={estilos.topo}/>
- 
- <Text style={estilos.titulo}>Detalhes do Carrionho</Text>
+ <Topo/>
 
- <View>
+   <View style={estilos.carrinho}>
+        <Text style={estilos.nome}>Carrinho de Compras</Text>
+        <View style={estilos.fazenda}>
+            <Image style={estilos.imagemFazenda} source={logo}/>
+            <Text style={estilos.nomeFazenda}>Fazenda do grande mestre Vinícius</Text>
+        </View>
     <Text> Carrinho de Compras </Text>   
     <Text> Fazendinha do incrivel Vinícus </Text>   
     <Text> Uma cesta com produtos selecionadas com cuidado e carinho</Text>   
@@ -23,21 +28,51 @@ export default function Carrinho() {
 }
 
 const estilos = StyleSheet.create({
-
-    topo: {
-        width: "100%",
-        height: 578 / 768 * width,
+  
+    titulo:{
+      width: "100%",
+      position: "absolute",
+      color: "white",
+      textAlign: "center",
+      fontSize: 24,
+      lineHeight: 26,
+      padding: 16,
+      fontFamily: "MontserratItalic"
     },
-    titulo: {
-        width: "100%",
-        position: "absolute",
-        color: "white",
-        textAlign: "center",
-        fontSize: 24,
-        lineHeight: 26,
-        padding: 16,
-        fontFamily: "MontserratItalic"
-    }
+    carrinho: {
+      paddingVertical: 8,
+      paddingHorizontal: 16,
+    },
 
+    nome: {
+      fontSize: 26,
+      lineHeight: 42,
+      color: "#464646"
+    },
+
+    fazenda: {
+      flexDirection: "row",
+      paddingVertical: 12
+    },
+    imagemFazenda:{
+      width: 32,
+      height: 32
+    },
+    nomeFazenda: {
+      fontSize: 16,
+      fontFamily: "MontserratBold",
+      lineHeight: 26,
+      marginLeft: 12
+    },
+    descricao: {
+      color: "#A3A3A3",
+      fontFamily: "MontserratItalic"
+    },
+    preco: {
+      fontSize: 26,
+      fontFamily: "MontserratRegular",
+      color: "#2A9f85",
+      marginTop: 8,
+
+    }
 })
-{/*Sou Gay*/}
---
+++ src/pages/components/Topo.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import { Dimensions, Image, StyleSheet, Text, View  } from "react-native";
+import topo from '../../../assets/topo.png';
+
+const width = Dimensions.get('screen').width;
+
+export default function Topo(){
+    return<>   
+    
+    <Image source={ topo } style={estilos.topo}/>
+ 
+ <Text style={estilos.titulo}>Detalhes do Carrionho</Text>
+    
+    
+        </>
+        
+}
+
+const estilos = StyleSheet.create({
+
+    topo: {
+        width: "100%",
+        height: 578 / 768 * width,
+    },
+    titulo: {
+        width: "100%",
+        position: "absolute",
+        color: "white",
+        textAlign: "center",
+        fontSize: 24,
+        lineHeight: 26,
+        padding: 16,
+        fontFamily: "MontserratItalic"
+    }
+
+})
